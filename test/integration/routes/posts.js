cb4ef1728f6f2f16b2ab9b58137957affe25888f
locemarn <marcelon27@gmail.com>
2019-06-02 12:26:38 -0300

    implementation of first test integration

---
+++ test/integration/routes/posts.js
@@ -0,0 +1,17 @@
+describe('Routes: Posts', () => {
+  const defaultPost = {
+    post: 'Default Post',
+    description: 'Default description',
+    author: 'Default author',
+    likes: 100
+  }
+
+  describe('GET /posts', () => {
+    it('should return a list of posts', done => {
+      request.get('/api/posts').end((err, res) => {
+        expect(res.body[0]).to.be.eql(defaultPost)
+        done(err)
+      })
+    })
+  })
+})
